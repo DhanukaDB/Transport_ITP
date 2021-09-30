88d6eeeb5bce7636f5b9d26282de0302501a036e
DhanukaDB <69480239+DhanukaDB@users.noreply.github.com>
2021-10-01 01:01:41 +0530

    Edited Vehicle Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,7 +9,7 @@ function Home(){
         
 <div>
  
-<Image src="https://res.cloudinary.com/hidl3r/image/upload/v1631381654/itp/LeBus-interior-luxury-coach-bus-background_pdzuvu.jpg" fluid />
+<Image src="https://res.cloudinary.com/hidl3r/image/upload/v1631381654/itp/LeBus-interior-luxury-coach-bus-background_pdzuvu.jpg" style={{maxWidth:'100%'}} fluid />
 
 <button className="time">Time</button>
 <button className="booknow">Book Now</button>
--- frontend/src/components/VehicleHome.js
+++ frontend/src/components/VehicleHome.js
@@ -1,6 +1,8 @@
 import React from "react";
-import {Button, Image} from "react-bootstrap";
+import {Button, Image,Container,Col,Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel'
+
 
 
 function VHome(){
@@ -16,12 +18,10 @@ function VHome(){
 <center><h1>Vehicle Admin</h1></center>
 <br/>
 
-
-<center><Image src="https://res.cloudinary.com/hidl3r/image/upload/v1631381664/itp/360_F_187684566_hroSDp0dkmSpZun0SJvETmuQxoN5dzEB_tjwwwx.jpg" fluid />
-</center>
-
-<br/>
-<Link to = "/addvehicle">
+<Container>
+  <Row>
+    <Col>
+    <br/><br/><br/><br/><br/><Link to = "/addvehicle">
   <Button variant="primary" size="lg" block>
   Add Vehicle
   </Button>
@@ -31,14 +31,44 @@ function VHome(){
   <Button variant="primary" size="lg" block>
   View All Vehicles Details
   </Button>
-  </Link>
+  </Link></Col>
+    <Col><Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src= "https://res.cloudinary.com/hidl3r/image/upload/v1633024712/itp/5f1d841ff90d14f3b318893282e3909a_ec7ynq.gif"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://res.cloudinary.com/hidl3r/image/upload/v1633024706/itp/2_wwwmkk.gif"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+ 
+</Carousel></Col>
+  </Row>
+
+</Container>
+
+
+<br/>
+
   
 
 
 
-
-
- 
 
 
  </div>
